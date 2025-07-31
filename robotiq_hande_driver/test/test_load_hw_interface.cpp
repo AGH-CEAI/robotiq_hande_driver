e4ae9d75fc32c790e98be75fba58fe23ac12886a
tylim2946 <50300906+tylim2946@users.noreply.github.com>
2025-07-30 17:05:30 -0700

    fix: fix typos in hw_system_gripper_1dof_

--- robotiq_hande_driver/test/test_load_hw_interface.cpp
+++ robotiq_hande_driver/test/test_load_hw_interface.cpp
@@ -27,9 +27,9 @@ class TestHWInterface : public ::testing::Test {
                 <hardware>
                     <plugin>robotiq_hande_driver/RobotiqHandeHardwareInterface</plugin>
                     <param name="grip_pos_min">0.0</param>
-                    <param name="grip_pos_max" default="0.025</param>
+                    <param name="grip_pos_max">0.025</param>
                     <param name="tty">/tmp/ttyUR</param>
-                    <param name="baudrate" >115200</param>
+                    <param name="baudrate">115200</param>
                     <param name="parity">N</param>
                     <param name="data_bits">8</param>
                     <param name="stop_bit">1</param>

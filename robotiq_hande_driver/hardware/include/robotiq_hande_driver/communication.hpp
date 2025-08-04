7da5b982963eaad7a42c3f06fb3dd83c58343142
macmacal <mac.aleksandrowicz@gmail.com>
2025-08-04 13:30:21 +0200

    communication refactor

--- robotiq_hande_driver/hardware/include/robotiq_hande_driver/communication.hpp
+++ robotiq_hande_driver/hardware/include/robotiq_hande_driver/communication.hpp
@@ -48,9 +48,7 @@ class Communication {
    public:
     Communication();
 
-    ~Communication() {
-        cleanup();
-    };
+    ~Communication();
 
     /**
      * @brief Initializes driver parameters.
@@ -71,63 +69,16 @@ class Communication {
         char parity,
         int data_bits,
         int stop_bit,
-        int slave_id) {
-        tty_port_ = tty_port.c_str();
-        baudrate_ = baudrate;
-        parity_ = parity;
-        data_bits_ = data_bits;
-        stop_bit_ = stop_bit;
-        slave_id_ = slave_id;
-    };
+        int slave_id);
 
     /**
      * @brief Reads and writes input/output registers at once.
      *
      * @param none
      * @return None.
-     * @note The status should be checked to verify successful execution. An exception is thrown if
-     * communication issues occur.
+     * @note Used in external thread.
      */
-    void read_write_registers() {
-        /**
-         * @brief Read and write modbus registers at once
-         *
-         * @param modbus_t *ctx
-         * @param int write_addr
-         * @param int write_nb
-         * @param uint16_t *src
-         * @param int read_addr
-         * @param int read_nb
-         * @param uint16_t *dest
-         * @return int FAILURE_MODBUS on failure, nonnegative value for success
-         * @note The status should be checked to verify successful execution. An exception is thrown
-         * if communication issues occur.
-         */
-        // int result;
-
-        while(running) {
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
-
-            for(size_t i = 0; i < NUM_OF_OUTPUT_BYTES; ++i) {
-                output_bytes_modbus_[i] = output_bytes_[i].load(std::memory_order_relaxed);
-            }
-
-            modbus_write_and_read_registers(
-                mb_,
-                GRIPPER_INPUT_FIRST_REG,
-                OUTPUT_REGISTER_WORD_LENGTH,
-                reinterpret_cast<uint16_t*>(output_bytes_modbus_),
-                GRIPPER_OUTPUT_FIRST_REG,
-                INPUT_REGISTER_WORD_LENGTH,
-                reinterpret_cast<uint16_t*>(input_bytes_modbus_));
-
-            for(size_t i = 0; i < NUM_OF_INPUT_BYTES; ++i) {
-                input_bytes_[i].store(input_bytes_modbus_[i], std::memory_order_relaxed);
-            }
-        }
-
-        // return result;
-    };
+    void read_write_registers();
 
     /**
      * @brief Initializes communication layer.
@@ -137,43 +88,22 @@ class Communication {
      * @note The status should be checked to verify successful execution. An exception is thrown if
      * communication issues occur.
      */
-    int configure() {
-        int result;
-
-        mb_ = modbus_new_rtu(tty_port_, baudrate_, parity_, data_bits_, stop_bit_);
-        modbus_set_slave(mb_, slave_id_);
-        modbus_set_debug(mb_, DEBUG_MODBUS);
-        result = connect();
-
-        my_thread.emplace(&Communication::read_write_registers, this);
-
-        return result;
-    };
+    int configure();
 
     /**
      * @brief Deinitializes communication layer.
      *
      * @param none
      * @return None.
-     * @note The status should be checked to verify successful execution. An exception is thrown if
-     * communication issues occur.
      */
-    void cleanup() {
-        if(my_thread && my_thread->joinable()) {
-            my_thread->join();
-        }
-
-        disconnect();
-        modbus_free(mb_);
-    };
+    void cleanup();
 
     /**
      * @brief Connects to the gripper using Modbus RTU and a virtual socket.
      *
      * @param none
      * @return connection status code.
-     * @note The status should be checked to verify successful execution. An exception is thrown if
-     * communication issues occur.
+     * @note The status should be checked to verify successful execution.
      */
     int connect();
 
@@ -182,39 +112,24 @@ class Communication {
      *
      * @param none
      * @return None.
-     * @note The status should be checked to verify successful execution. An exception is thrown if
-     * communication issues occur.
      */
-    void disconnect() {
-        modbus_close(mb_);
-    };
+    void disconnect();
 
     /**
      * @brief Sets output bytes to zeros.
      *
      * @param none
      * @return None.
-     * @note The status should be checked to verify successful execution. An exception is thrown if
-     * communication issues occur.
      */
-    void clear_output_bytes() {
-        // memset(output_bytes_, 0, sizeof(output_bytes_));
-        for(size_t i = 0; i < static_cast<size_t>(OutputBytes::BYTES_MAX); ++i) {
-            output_bytes_[i].store(0, std::memory_order_relaxed);
-        }
-    };
+    void clear_output_bytes();
 
     /**
      * @brief Retrieves the input byte value at the specified index.
      *
      * @param index The InputBytes byte index.
      * @return Requested byte value.
-     * @note The status should be checked to verify successful execution. An exception is thrown if
-     * communication issues occur.
      */
-    uint8_t get_input_byte(InputBytes index) {
-        return input_bytes_[static_cast<uint>(index)].load(std::memory_order_relaxed);
-    };
+    uint8_t get_input_byte(InputBytes index);
 
     /**
      * @brief Sets the output byte value at the specified index.
@@ -222,12 +137,8 @@ class Communication {
      * @param index OutputBytes byte index
      * @param value The value to be set.
      * @return none
-     * @note The status should be checked to verify successful execution. An exception is thrown if
-     * communication issues occur.
      */
-    void set_output_byte(OutputBytes index, uint8_t value) {
-        output_bytes_[static_cast<uint>(index)].store(value, std::memory_order_relaxed);
-    };
+    void set_output_byte(OutputBytes index, uint8_t value);
 
     /**
      * @brief Sets the n-th bit of the action request byte.
@@ -236,15 +147,7 @@ class Communication {
      * @param value The boolean value to set for the bit.
      * @return None.
      */
-    void write_action_bit(uint8_t position_bit, bool value) {
-        output_bytes_[static_cast<uint>(OutputBytes::ACTION_REQUEST)].store(
-            bit_set_to(
-                output_bytes_[static_cast<uint>(OutputBytes::ACTION_REQUEST)].load(
-                    std::memory_order_relaxed),
-                position_bit,
-                value),
-            std::memory_order_relaxed);
-    };
+    void write_action_bit(uint8_t position_bit, bool value);
 
     /**
      * @brief Sets the n-th bit of a number to the specified boolean value.
@@ -254,11 +157,7 @@ class Communication {
      * @param x The boolean value to set the bit to.
      * @return The modified number with the n-th bit set to the specified value.
      */
-    uint bit_set_to(uint value, uint n, bool x) {
-        uint reset_n_bit = ~(1u << n);
-        uint set_n_bit = static_cast<uint>(x) << n;
-        return (value & reset_n_bit) | set_n_bit;
-    };
+    uint bit_set_to(uint value, uint n, bool x);
 
    private:
     const char* tty_port_;
@@ -270,14 +169,14 @@ class Communication {
 
     modbus_t* mb_;
 
-    uint8_t input_bytes_modbus_[static_cast<size_t>(InputBytes::BYTES_MAX)];
-    uint8_t output_bytes_modbus_[static_cast<size_t>(OutputBytes::BYTES_MAX)];
+    uint8_t input_bytes_modbus_[NUM_OF_INPUT_BYTES];
+    uint8_t output_bytes_modbus_[NUM_OF_OUTPUT_BYTES];
 
-    std::atomic<uint8_t> input_bytes_[static_cast<size_t>(InputBytes::BYTES_MAX)];
-    std::atomic<uint8_t> output_bytes_[static_cast<size_t>(OutputBytes::BYTES_MAX)];
+    std::atomic<uint8_t> input_bytes_[NUM_OF_INPUT_BYTES];
+    std::atomic<uint8_t> output_bytes_[NUM_OF_OUTPUT_BYTES];
 
-    std::atomic<bool> running{true};
-    std::optional<std::thread> my_thread;
+    std::atomic<bool> bg_comm_enabled_{true};
+    std::optional<std::thread> bg_comm_;
 };
 }  // namespace robotiq_hande_driver
 #endif  // ROBOTIQ_HANDE_DRIVER__COMMUNICATION_HPP_
--- robotiq_hande_driver/hardware/src/communication.cpp
+++ robotiq_hande_driver/hardware/src/communication.cpp
@@ -7,13 +7,105 @@ namespace robotiq_hande_driver {
 Communication::Communication()
     : input_bytes_modbus_{}, output_bytes_modbus_{}, input_bytes_{}, output_bytes_{} {}
 
+Communication::~Communication() {
+    cleanup();
+}
+
+void Communication::initialize(
+    const std::string& tty_port,
+    int baudrate,
+    char parity,
+    int data_bits,
+    int stop_bit,
+    int slave_id) {
+    tty_port_ = tty_port.c_str();
+    baudrate_ = baudrate;
+    parity_ = parity;
+    data_bits_ = data_bits;
+    stop_bit_ = stop_bit;
+    slave_id_ = slave_id;
+}
+
+void Communication::read_write_registers() {
+    while(bg_comm_enabled_) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+        for(size_t i = 0; i < NUM_OF_OUTPUT_BYTES; ++i) {
+            output_bytes_modbus_[i] = output_bytes_[i].load(std::memory_order_relaxed);
+        }
+
+        modbus_write_and_read_registers(
+            mb_,
+            GRIPPER_INPUT_FIRST_REG,
+            OUTPUT_REGISTER_WORD_LENGTH,
+            reinterpret_cast<uint16_t*>(output_bytes_modbus_),
+            GRIPPER_OUTPUT_FIRST_REG,
+            INPUT_REGISTER_WORD_LENGTH,
+            reinterpret_cast<uint16_t*>(input_bytes_modbus_));
+
+        for(size_t i = 0; i < NUM_OF_INPUT_BYTES; ++i) {
+            input_bytes_[i].store(input_bytes_modbus_[i], std::memory_order_relaxed);
+        }
+    }
+}
+
+int Communication::configure() {
+    mb_ = modbus_new_rtu(tty_port_, baudrate_, parity_, data_bits_, stop_bit_);
+    modbus_set_slave(mb_, slave_id_);
+    modbus_set_debug(mb_, DEBUG_MODBUS);
+    auto result = connect();
+
+    bg_comm_.emplace(&Communication::read_write_registers, this);
+
+    return result;
+}
+
+void Communication::cleanup() {
+    bg_comm_enabled_.store(false, std::memory_order_relaxed);
+    if(bg_comm_ && bg_comm_->joinable()) {
+        bg_comm_->join();
+    }
+
+    disconnect();
+    modbus_free(mb_);
+}
+
 int Communication::connect() {
-    // int result;
+    return modbus_connect(mb_);
+}
 
-    modbus_connect(mb_);
+void Communication::disconnect() {
+    modbus_close(mb_);
+}
 
-    // read_write_registers();
+void Communication::clear_output_bytes() {
+    for(size_t i = 0; i < NUM_OF_OUTPUT_BYTES; ++i) {
+        output_bytes_[i].store(0, std::memory_order_relaxed);
+    }
+    memset(output_bytes_modbus_, 0, sizeof(output_bytes_modbus_));
+}
 
-    return 0;
+uint8_t Communication::get_input_byte(InputBytes index) {
+    auto idx = static_cast<uint>(index);
+    return input_bytes_[idx].load(std::memory_order_relaxed);
+}
+
+void Communication::set_output_byte(OutputBytes index, uint8_t value) {
+    auto idx = static_cast<uint>(index);
+    output_bytes_[idx].store(value, std::memory_order_relaxed);
+}
+
+void Communication::write_action_bit(uint8_t position_bit, bool value) {
+    auto idx = static_cast<uint>(OutputBytes::ACTION_REQUEST;
+    auto reg = output_bytes_[idx].load(std::memory_order_relaxed);
+    auto result = bit_set_to(reg, position_bit, value);
+
+    output_bytes_[idx].store(result, std::memory_order_relaxed);
+}
+
+uint Communication::bit_set_to(uint value, uint n, bool x) {
+    uint reset_n_bit = ~(1u << n);
+    uint set_n_bit = static_cast<uint>(x) << n;
+    return (value & reset_n_bit) | set_n_bit;
 }
 }  // namespace robotiq_hande_driver

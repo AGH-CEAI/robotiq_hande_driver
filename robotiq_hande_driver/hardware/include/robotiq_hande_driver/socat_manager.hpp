b04ed2b8917b475e2983ef9a22189f48bc248a55
macmacal <mac.aleksandrowicz@gmail.com>
2025-08-13 20:36:24 +0200

    calibrated socat wait

--- robotiq_hande_driver/hardware/include/robotiq_hande_driver/socat_manager.hpp
+++ robotiq_hande_driver/hardware/include/robotiq_hande_driver/socat_manager.hpp
@@ -13,7 +13,7 @@
 
 namespace robotiq_hande_driver {
 
-static constexpr auto WAIT_FOR_SOCAT = std::chrono::milliseconds(100);
+static constexpr auto WAIT_FOR_SOCAT = std::chrono::milliseconds(1000);
 
 class SocatManager {
    public:

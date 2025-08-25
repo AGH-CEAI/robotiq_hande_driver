1ebafe3e563c299cb17472f6c71ff70b42617401
Jakub <sivral@proton.me>
2025-08-25 17:39:16 +0200

    Improve README setup instructions (#27)

--- robotiq_hande_driver/hardware/src/hande_hardware_interface.cpp
+++ robotiq_hande_driver/hardware/src/hande_hardware_interface.cpp
@@ -37,8 +37,8 @@ HWI::CallbackReturn RobotiqHandeHardwareInterface::on_init(const HWI::HardwareIn
     bool manage_virutal_serial = str_to_lower(info_.hardware_parameters["create_socat_tty"])
                                  == "true";
     if(manage_virutal_serial) {
-        auto ip_addr = info_.hardware_parameters["ip_adress"];
-        auto port = info_.hardware_parameters["port"];
+        auto ip_addr = info_.hardware_parameters["socat_ip_address"];
+        auto port = info_.hardware_parameters["socat_port"];
         auto tty_port = info_.hardware_parameters["tty_port"];
 
         RCLCPP_INFO(

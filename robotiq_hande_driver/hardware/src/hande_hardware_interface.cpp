dbde7b7527db956450f57b98caeafa260d9658a8
Jakub <sivral@proton.me>
2025-11-23 22:07:44 +0100

    Wait for ready state before starting communication

--- robotiq_hande_driver/hardware/src/hande_hardware_interface.cpp
+++ robotiq_hande_driver/hardware/src/hande_hardware_interface.cpp
@@ -186,6 +186,11 @@ HWI::CallbackReturn RobotiqHandeHardwareInterface::on_activate(
             th_comm_enabled_.store(true, std::memory_order_relaxed);
             th_comm_.emplace(&RobotiqHandeHardwareInterface::gripper_communication, this);
         }
+
+        RCLCPP_INFO(get_logger(), "%sWaiting for Hand-E complete activation%s", color::BCYAN, color::RESET);
+        while(!gripper_driver_.get_status().is_ready) {
+            std::this_thread::sleep_for(th_sleep_rate_);
+        }
     } catch(const std::exception& e) {
         RCLCPP_ERROR(
             get_logger(),
